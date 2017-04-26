c31cf2fbc53b3bbdff960d6d4f097eae0c210987
setherapptor <rappsh@mail.uc.edu>
2017-04-26 12:46:46 -0500

    Worked on room design

--- Final_Project_Game/room.py
+++ Final_Project_Game/room.py
@@ -73,7 +73,7 @@ class Room2(Room):
         self.block_list.add(Block(pos = (640,192), moving = (2,640,256,0,0,0)))
         self.block_list.add(Block(pos = (64,384)))
 
-#Complete
+
 class Room3(Room):
     def __init__(self):
         super().__init__()
@@ -101,12 +101,38 @@ class Room4(Room):
         self.left = None
         self.right = None
         self.up = 3
+        self.down = 5
+
+        i = 0
+        while i < constants.SCREEN_WIDTH:
+            if i <= 384:
+                self.block_list.add(Block(pos = (i,0)))
+            #self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-64)))
+            self.block_list.add(Block(pos = (448, i)))
+            if i > 128:
+                self.block_list.add(Block(pos = (320,i)))
+            if i != 384:
+                self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-64)))
+            i += 64
+        self.block_list.add(Block(pos = (512,384), moving = (2,704,512,0,0,0)))
+        self.block_list.add(Block(pos = (0,256), moving = (4,192,0,0,0,0)))
+
+class Room5(Room):
+    def __init__(self):
+        super().__init__()
+        self.id = 5
+        self.left = None
+        self.right = None
+        self.up = 4
         self.down = None
 
         i = 0
         while i < constants.SCREEN_WIDTH:
-            #self.block_list.add(Block(pos = (i,0)))
-            #self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-64)))
-            self.block_list.add(Block(pos = (448, i)))
+            if i != 384:
+                self.block_list.add(Block(pos = (i, 0)))
             self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-64)))
+            if i >= 64 and i <= 640:
+                self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-128)))
+            if i >= 128 and i <= 576:
+                self.block_list.add(Block(pos = (i, constants.SCREEN_HEIGHT-192)))
             i += 64
