123a14e9c0de3e648000156392553b37a551015f
Evan Shearer <evanmshearer@gmail.com>
2017-04-26 12:53:14 -0400

    ensgame.py commenting and removing unused code

--- Final_Project_Game/ensgame.py
+++ Final_Project_Game/ensgame.py
@@ -1,22 +1,30 @@
+#ensgame.py
+#Contains the main and initializes both the Game and Pygame
+#ensgame stands for Evan and Seth's game
+
 import pygame as p
 from game import Game
-from item import Item
-import color
 import constants
 
 def main():
+  #Initializes pygame
   p.init()
   
+  #Creates a display that the game prints objects to
   size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
   display = p.display.set_mode(size)
   
+  #Sets window's name and icon
   p.display.set_caption('Evan and Seth\'s Game!')
   p.display.set_icon(p.image.load('Images\\icon.png'))
   
+  #If True, the game closes and ends
   done = False
   
+  #Clock lets the game pause for a tiny bit so that the target framerate is not exceeded
   clock = p.time.Clock()
   
+  #initializes Game from game.py
   game = Game()
 
   while not done:
@@ -24,7 +32,7 @@ def main():
     game.logic()
     game.draw_frame(display)
     
-    clock.tick(60)
+    clock.tick(60)#Target framerate is 60 frames per second
     
   p.quit()
   
